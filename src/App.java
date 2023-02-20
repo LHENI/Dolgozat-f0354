451577b4366e1400c78955c533d84e5700fba1cb
LHENI <lheni644@gmail.com>
2023-02-20 12:56:34 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+/*
+* File: App.java
+* Author: Laczkó Henrietta
+* Copyright: 2023, Laczkó Henrietta
+* Group: Szoft I-I-N
+* Date: 2023-02-20
+* Github: https://github.com/LHENI/Dolgozat-f0354.git
+* Licenc: GNU GPL
+*/
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
---
+++ src/Area.java
@@ -0,0 +1,13 @@
+/*
+* File: Area.java
+* Author: Laczkó Henrietta
+* Copyright: 2023, Laczkó Henrietta
+* Group: Szoft I-I-N
+* Date: 2023-02-20
+* Github: https://github.com/LHENI/Dolgozat-f0354.git
+* Licenc: GNU GPL
+*/
+
+public class Area {
+    
+}
